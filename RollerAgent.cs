4f742c12e6f82cf56721d3f7299eeb16bfcb96b8
Jamie Gardner <jamiedanielgrdner@gmail.com>
2020-07-23 09:44:35 +0100

    Configure for Ml agent

--- RollerAgent.cs
+++ RollerAgent.cs
@@ -1,17 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
 
-public class RollerAgent : MonoBehaviour
+public class RollerAgent : Agent
 {
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
