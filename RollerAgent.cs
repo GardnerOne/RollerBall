775c13c559aac8d713d665fbdeee4e277862efa4
Jamie Gardner <jamiedanielgrdner@gmail.com>
2020-07-23 10:01:19 +0100

    Reset the environment for each episode

--- RollerAgent.cs
+++ RollerAgent.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections.Specialized;
+using System.Numerics;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
@@ -6,9 +9,29 @@ using Unity.MLAgents.Sensors;
 
 public class RollerAgent : Agent
 {
+    RigidBody rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<RigidBody>();
+    }
+
+    public Transform Target;
+
+    public override void OnEpisodeBegin()
+    {
+        if (this.transform.localPosition.y < 0)
+        {
+            // If the Agent fell, zero its momentum
+            this.rigidBody.angularVelocity = Vector3.zero;
+            this.rigidBody.velocity = Vector3.zero;
+            this.transform.localPosition = new Vector3(0, 0.5f, 0);
+        }
+
+        // Move target to new spot
+        Target.localPosition = new Vector3(Random.value * 8 - 4,
+                                           0.5f,
+                                           Random.value * 8 - 4);
     }
 }
